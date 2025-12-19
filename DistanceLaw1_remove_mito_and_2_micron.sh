1077bc15a9583aa0a5baaabd7eac1b3cd9efb87a
Piazza lab <46714532+piazza-lab@users.noreply.github.com>
2025-12-19 15:42:38 +0100

    Delete DistanceLaw1_remove_mito_and_2_micron.sh

--- DistanceLaw1_remove_mito_and_2_micron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sample=$1
-awk '!/mitochondrion/' $sample'.txt' > tmpfile && mv tmpfile $sample'_woMito.txt';
-awk '!/2_micron/' $sample'_woMito.txt' > tmpfile && mv tmpfile $sample'_chr.txt';
